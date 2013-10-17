153b31fc538a70c84fb534ec9a850751ef4fe096
Peter Alstone <peter.alstone@gmail.com>
2013-10-17 10:46:49 -0700

    my updates...

--- ps1/kd-by-hand.R
+++ ps1/kd-by-hand.R
@@ -13,37 +13,20 @@ data.sm <- with(ps1.data.clean[smokelist,], data.frame(birth.weight = dbrwt, wei
 
 data.ns <- with(ps1.data.clean[nonsmokelist,], data.frame(birth.weight = dbrwt, weight = 1/(1-propensityreduced)))
 
-kd.est <- function(data, x = "x", weight = "weight", x.val, kernel = "epa"){
-  # data is a dataframe with x-values and weights
-  # x is the name of x-variables
-  # weight is the weights for each
-  # x.val is the particular value of x that is being evaluated
-  # kernel is the kernel type (ONLY EPA NOW)
-  
-  
-  
-  
-}
-
 ps3k.sm <- with(ps1.data.clean, mean(propensityreduced[which(dbrwt == 3000 & tobacco.rescale == 1)]))
 ps3k.ns <- with(ps1.data.clean, mean(propensityreduced[which(dbrwt == 3000 & tobacco.rescale == 0)]))
 
+# find smoking at 3000
+
+# find vector of contributions to kernel weight, weighted by inverse p-score
+for(i in 1:length(data.sm$birth.weight)){
+  data.sm$ksum[i] <- data.sm$weight[i] * kernel.epa((data.sm$birth.weight[i] - 3000) / h)
+}
+
+reasonable.weight <- which(data.sm$weight < 10)
+
+# sum contributions and divide by number of obs., 
+kernel.3000.sm <- sum(data.sm$ksum) / (sum(data.sm$weight[reasonable.weight]) * length(data.sm$ksum) * h)
 
 
 
-##for(i in 1:nrow(subset(ps1.data.clean, tobacco.rescale == 1))){
-##with(subset(ps1.data.clean, tobacco.rescale == 1),
-##     kern3000.sm.num <- kern3000.sm.num +
-##      kernel.epa(((propensity3000.sm-propensityreduced[i])/h)*dbrwt))
-## with(subset(ps1.data.clean, tobacco.rescale == 1),
-##      kern3000.sm.den <- kern3000.sm.den +
-##      kernel.epa((propensity3000.sm-propensityreduced[i])/h))
-## }
-## kern3000.sm <- kern3000.sm.num / kern3000.sm.den
-
-
-## kernel3000.sm <- with(subset(ps1.data.clean,tobacco.rescale == 1), data.frame(window = (3000 - dbrwt/h)))
-## kernel3000.sm$numerator <- with(subset(ps1.data.clean, tobacco.rescale == 1), kernel.epa(((3000/propensity3000.sm) - (dbrwt/propensityreduced))/h))
-## kernel3000.sm$denominator <- with(subset(ps1.data.clean, tobacco.rescale == 1), kernel.epa(((3000/propensity3000.sm) - (dbrwt/propensityreduced))/h))
-
-## with(kernel3000.sm[window < 1 & window > -1], sum(numerator))/(nrow(kernel3000.sm[abs(window < 1)])*h)
