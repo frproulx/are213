fa5402c768eb08c0b44bfdcbecefa0d146ec81ee
Peter Alstone <peter.alstone@gmail.com>
2013-11-05 15:49:37 -0800

    Getting close  - todo in comments
    Done with the core analyses and have some text in place for describing
    SE math.  Still a bit fuzzy on it but it is there...
    
    Also, got the sqr.year term to work OK, so it is incorporated.
    
    Still TODO: Hand code robust clustered SE for 3b (there is a blog that
    does this linked in the code...maybe adopt this?).
    
    Also: The handwritten problems 1 and 2 are relatively weak right now.
    I would say my versions get about 2/3 of the way there.  Matrix
    operations are not my strong point.

--- ps2/ps2a-paworking.r
+++ ps2/ps2a-paworking.r
@@ -3,6 +3,8 @@ setwd("/media/frank/Data/documents/school/berkeley/fall13/are213/are213/ps2")
 ## Peter's wd
 # setwd("~/Google Drive/ERG/Classes/ARE213/are213/ps2")
 
+# TODO: Hand-code an OLS clustered estimator for Prob 3b.  One option for this is to reference and adopt the code at http://diffuseprior.wordpress.com/2012/06/15/standard-robust-and-clustered-standard-errors-computed-in-r
+
 library(foreign) #this is to read in Stata data
 library(Hmisc)
 library(psych)
@@ -24,12 +26,25 @@ ps2a.data$logfatalpc <- with(ps2a.data, log(fatalities/population))
 ps2a.data$sqyears <- with(ps2a.data, year^2)
 
 # Graphical exploration of data
+pdf(file="allstates.pdf", width = 7, height = 7.5)
+ggplot(ps2a.data, aes(year, fatalities/population)) + 
+  geom_point(aes(color=primary)) + 
+  scale_x_continuous(breaks = c(1985, 1995)) +
+  scale_y_continuous(breaks = c(0.2, 0.4)) +
+  facet_wrap("state") + 
+  ylab("log(fatalities) per capita") +
+  ggtitle("State-level time series in log of fatalities per capita") +
+  stat_smooth()
+dev.off()
 
-ggplot(ps2a.data, aes(year, fatalities/population)) + geom_point(aes(color=primary)) + facet_grid(.~primary) + stat_smooth()
-
+#plot 3a
+makeplot3a <- FALSE
+if(makeplot3a){
 pdf("plot3a.pdf", width=5, height=4)
 ggplot(ps2a.data, aes(year, log(fatalities/population))) + geom_point(aes(color=primary)) + facet_grid(.~primary) + stat_smooth()
 dev.off()
+}
+
 
 #Puts data into pdata.frame for use with the plm package.
 ps2a.pdata <- pdata.frame(ps2a.data, index = c("state", "year")) 
@@ -39,12 +54,15 @@ ps2a.pdata <- pdata.frame(ps2a.data, index = c("state", "year"))
 # Issues with non-conforming matrix seem to trace to bad factor class for year....fixed here.  
 ps2a.pdata$year <- as.numeric(ps2a.pdata$year)
 
-## Part A (pooled OLS, quadratic time trend, and all possible covariates)
+# ADDDING square year term HERE WORKS FOR WHATEVER REASON
+ps2a.pdata$sqr.year <- with(ps2a.pdata, year^2)
+
+## Part A (pooled OLS, quadratic time trend, and all possible covariates) ------
 pooled.OLS <- plm(logfatalpc ~ primary, data = ps2a.pdata, model = "pooling")
 
-pooled.quadtime <- plm(logfatalpc ~ primary + year + sqyears, data = ps2a.pdata, model = "pooling")
+pooled.quadtime <- plm(logfatalpc ~ primary + year + sqr.year, data = ps2a.pdata, model = "pooling")
 
-pooled.full <- plm(logfatalpc ~ primary + year + sqyears + secondary + college + beer + totalvmt + precip + snow32 + rural_speed + urban_speed, data = ps2a.pdata, model = "pooling")
+pooled.full <- plm(logfatalpc ~ primary + year + sqr.year + secondary + college + beer + totalvmt + precip + snow32 + rural_speed + urban_speed, data = ps2a.pdata, model = "pooling")
 
 stargazer(pooled.OLS, pooled.quadtime, pooled.full,
           title = "Pooled Models of Fatalities Per Capita", 
@@ -54,32 +72,58 @@ stargazer(pooled.OLS, pooled.quadtime, pooled.full,
           column.labels = c("bivariate", "quadratic time", "covariates"),
           label="tab:3a")
 
+## Part B (Standard Error Errors) --------
+
 #typical
-coeftest(pooled.full)
+a.typ.full <- coeftest(pooled.full)
 #robust
-coeftest(pooled.full, vcov = vcovHC)
+a.robust.full <- coeftest(pooled.full, vcov = vcovHC)
 #clustered
-coeftest(pooled.full, vcov = vcovHC(pooled.full, type="HC1", cluster = "group"))
+a.clust.full <- coeftest(pooled.full, vcov = vcovHC(pooled.full, type="HC1", cluster = "group"))
 
-## Part B
-pooled.OLS.robust <- robust(pooled.OLS)
-pooled.quadtime.robust <- robust(pooled.quadtime)
-pooled.full.robust <- robust(pooled.full)
+stargazer(a.typ.full, a.robust.full, a.clust.full, 
+          title = "Comparison of Standard Error HC Methods for Full Pooled Model",
+          style = "qje",
+          out = 'p3b1.tex',
+          font.size = "footnotesize", 
+          column.labels = c("Conventional", "HC1 Robust", "HC1 Robust + Cluster"),
+          label="tab:3b1"
+          )
 
-# In theory you can get robust SE from adding "robust=TRUE" to a summary call of a plm object.  
-summary(pooled.OLS, robust=TRUE)
+# # OLS CASE doesn't work for some reason. ------
+# #typical
+# a.typ.OLS <- coeftest(pooled.OLS)
+# #robust
+# a.robust.OLS <- coeftest(pooled.OLS, vcov = vcovHC)
+# #clustered
+# a.clust.OLS <- coeftest(pooled.OLS, vcov = vcovHC(pooled.OLS, type="HC1", cluster = "group"))
+# 
+# stargazer(a.typ.OLS, a.robust.OLS, a.clust.OLS, 
+#           title = "Comparison of Standard Error HC Methods for Bivariate Pooled OLS",
+#           style = "qje",
+#           out = 'p3b1b.tex',
+#           font.size = "footnotesize", 
+#           column.labels = c("Assume Homo", "HC1 Robust", "HC1 Robust + Cluster"),
+#           label="tab:3b1b"
+# )
+# OLD CODE FROM Part B ------
+
+# pooled.OLS.robust <- robust(pooled.OLS)
+# pooled.quadtime.robust <- robust(pooled.quadtime)
+# pooled.full.robust <- robust(pooled.full)
+
+# # In theory you can get robust SE from adding "robust=TRUE" to a summary call of a plm object.  
+# summary(pooled.OLS, robust=TRUE)
 
 ## still needs cluster standard error
 
 # See http://people.su.se/~ma/clustering.pdf -- functions in util pulled from there...but doesn't work with plm package :(
 # Clustered SE code not included in basic R pacakge so need to hard-code it :)  Based on:
 # http://diffuseprior.wordpress.com/2012/06/15/standard-robust-and-clustered-standard-errors-computed-in-r/
-
-# This function only works in the bivariate case...
-ols.hetero(logfatalpc ~ primary, data = ps2a.data, robust=TRUE, cluster="state")
-
-
-
+# 
+# # This function only works in the bivariate case...
+# ols.hetero(logfatalpc ~ primary, data = ps2a.data, robust=TRUE, cluster="state")
+# 
 # ### REBOOT:  but this section doesn't work ------
 # 
 # # centering function
@@ -134,6 +178,8 @@ ols.hetero(logfatalpc ~ primary, data = ps2a.data, robust=TRUE, cluster="state")
                    
 
                    
+
+
 ## Part C: compute between estimator w/ and w/o covariates-----
 between.nocov <- plm(logfatalpc ~ primary, data = ps2a.pdata, model = "between")
 between.cov <- plm(logfatalpc ~ primary + secondary + college + totalvmt + snow32 + rural_speed, data = ps2a.pdata, model = "between")
@@ -148,10 +194,43 @@ stargazer(RE.nocov, RE.cov, title = "Random Effects Models", out = "p3d.tex", fo
 
 ## Part E compute cluster standard errors for RE
 
+e.typ.recov <- coeftest(RE.cov)
+e.clust.recov <- coeftest(RE.cov, vcov = vcovHC(RE.cov, type = "HC1", cluster = "group"))
+
+stargazer(e.typ.recov, e.clust.recov, 
+          title = "Comparison of Standard Error HC Methods for RE model with covariates",
+          style = "qje",
+          out = 'p3e.tex',
+          font.size = "footnotesize", 
+          column.labels = c("Conventional SE", "HC1 Robust + Cluster"),
+          label="tab:p3e"
+)
+
+
+
 ## Part F compute FE estimator
-fixed.primary <- plm(logfatalpc ~ primary + sqnetyears, data = ps2a.pdata, model = "within")
+fixed.primary <- plm(logfatalpc ~ primary + year + sqr.year, data = ps2a.pdata, model = "within")
+
+f.typ.fe <- coeftest(fixed.primary)
+f.clust.fe <- coeftest(fixed.primary, vcov = vcovHC(fixed.primary, type='HC1', cluster = "group"))
+
+stargazer(f.typ.fe, f.clust.fe, 
+          title = "Comparison of Standard Error HC Methods for basic FE model",
+          style = "qje",
+          out = 'p3f.tex',
+          font.size = "footnotesize", 
+          column.labels = c("Conventional SE", "HC1 Robust + Cluster"),
+          label="tab:p3f"
+)
+
 
 ## Part G
-fixed.cov <- plm(logfatalpc ~ primary + sqnetyears + secondary + college + beer + totalvmt + precip + snow32 + rural_speed + urban_speed, data = ps2a.pdata, model = "within")
+fixed.cov <- plm(logfatalpc ~ primary + year + sqr.year + secondary + college + beer + totalvmt + precip + snow32 + rural_speed + urban_speed, data = ps2a.pdata, model = "within")
 
-stargazer(fixed.primary, fixed.cov, title = "Fixed Effects Models", out = "p3fg.tex", font.size = "footnotesize")
+stargazer(fixed.primary, fixed.cov, 
+          coeftest(fixed.cov, vcov = vcovHC(fixed.cov, type="HC1", cluster = "group")),
+          title = "Fixed Effects Models", 
+          out = "p3g.tex", font.size = "footnotesize",
+          label = "tab:p3g",
+          column.labels = c("Simple FE", "FE with Cov.", "Robust/Clustered SE")
+          )
